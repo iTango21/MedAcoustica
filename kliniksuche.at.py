d8a6a718ae23f0f389ab47673481fe60d169d759
iTango21 <itango21@gmail.com>
2022-10-10 09:21:07 +0300

    Part 1...

--- kliniksuche.at.py
+++ kliniksuche.at.py
@@ -4,5 +4,170 @@ import lxml
 
 import json
 
+from fake_useragent import UserAgent
+
+# from random import randrange
+ua = UserAgent()
+ua_ = ua.random
+
+print('start...')
+
+url1 = 'https://khstrukturdaten.goeg.at/backend2/KliniksucheV2/api/spitaeler?'
+
+types_ = ['Gemeinnütziges Krankenhaus', 'Privatkrankenhaus']
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "User-Agent": f'{ua}'  # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
+    # like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+}
+
+with requests.Session() as session:
+    response = session.get(url=url1, headers=headers)
+
+    aaa = json.loads(response.text)
+
+    print(aaa)
+    print('--------------')
+
+hospital_info = []
+
+cou_ = 0
+
+
+for i in aaa:
+    cou_ += 1
+    nummer = i['nummer']
+    print(f'{cou_} ++++++++++++++++++++')
+    # nummer = 901
+    url2 = f'https://khstrukturdaten.goeg.at/backend2/KliniksucheV2/api/spitaeler/{nummer}'
+
+    with requests.Session() as session:
+        response = session.get(url=url2, headers=headers)
+        bbb = json.loads(response.text)
+
+        name_ = bbb['name']
+        url_ = f"https://kliniksuche.at/klinik/{bbb['nummer']}"
+
+        address_ = f"{bbb['adresse']['strasseHausnummer']} {bbb['adresse']['plzOrt']}"
+        tel_ = bbb['kontakt']['telefon']
+        fax_ = bbb['kontakt']['fax']
+        email_ = bbb['kontakt']['email']
+        website_ = bbb['kontakt']['internet']
+        # -----------------------------------
+        type__ = bbb['typ']
+        if type__ == 1:
+            type_ = types_[0]
+        else:
+            type_ = types_[1]
+        # -----------------------------------
+        lastUpdatedDate_ = bbb['lastUpdatedDate']
+
+        # positions_ = ['Direktion', 'Ärztliche Leitung', 'Pflegedienst-leitung', 'Verwaltungs-leitung', 'Technische Leitung']
+        # positions_ = ['direktion', 'aerztlicheLeitung', 'pflegedienstleitung', 'verwaltungsleitung', 'technischeLeitung']
+
+        departments_ = []
+        abteilungsleitung_ = []
+
+        for w in bbb['abteilungen']:
+
+            id__ = w['id']
+            # id__ = '5d9be90f-7c1d-4019-b0b8-494c3e74a08e'
+            print(f'\t{id__}')
+
+            url3 = f'https://khstrukturdaten.goeg.at/backend2/KliniksucheV2/api/spitaeler/901/abteilungen/{id__}'
+
+            with requests.Session() as session:
+                response = session.get(url=url3, headers=headers)
+                ccc = json.loads(response.text)
+
+            # print(f'{cou_}***************************')
+            # print(ccc['name'])
+            url_ccc = f'https://kliniksuche.at/klinik/901/abteilungen/{id__}'
+
+            abteilungsleitung_.append(
+                {
+                    "leitung": ccc['abteilungsleitung']['leitung'],
+                    "telefon": ccc['abteilungsleitung']['telefon'],
+                    "fax": ccc['abteilungsleitung']['fax'],
+                    "email": ccc['abteilungsleitung']['email'],
+                    "internet": ccc['abteilungsleitung']['internet']
+                }
+            )
+
+            betten_ = ccc['betten']['betten']
+
+            personal_ = []
+            for k, v in ccc['personal'].items():
+                personal_.append(
+                    {
+                        "name": k,
+                        "value": v
+                    }
+                )
+
+            aktuelleLeistungsschwerpunkte_ = ccc['leistungsschwerpunkte']
+
+            diagnosen_ = []
+            for v in ccc['diagnosen']:
+                diagnosen_.append(
+                    {
+                        "d": v
+                    }
+                )
+
+            hospital_info.append(
+                {
+                    "name": "Landeskrankenhaus Hartberg",
+                    "url": "https://kliniksuche.at/klinik/631",
+                    "contactInformation": {
+                        "address": address_,
+                        "tel": tel_,
+                        "fax": fax_,
+                        "email": email_,
+                        "website": website_,
+                        "type": type_,
+                        "lastUpdatedDate": lastUpdatedDate_
+                    },
+                    "managerNames": [
+                        {
+                            "position": "Direktion",
+                            "name": bbb['leitung']['direktion'],
+                        },
+                        {
+                            "position": "Ärztliche Leitung",
+                            "name": bbb['leitung']['aerztlicheLeitung'],
+                        },
+                        {
+                            "position": "Pflegedienst-leitung",
+                            "name": bbb['leitung']['pflegedienstleitung'],
+                        },
+                        {
+                            "position": "Verwaltungs-leitung",
+                            "name": bbb['leitung']['verwaltungsleitung'],
+                        },
+                        {
+                            "position": "Technische Leitung",
+                            "name": bbb['leitung']['technischeLeitung']
+                        }
+                    ],
+                    "departments": [
+                        {
+                            "name": ccc['name'],
+                            "url": url_ccc,
+                            "abteilungsleitung": abteilungsleitung_
+
+                         }
+
+                    ],
+                    "betten": betten_,
+                    "personal": personal_,
+                    "aktuelleLeistungsschwerpunkte": aktuelleLeistungsschwerpunkte_,
+                    "diagnosen": diagnosen_
+                }
+            )
+
+with open(f'___Kliniksuche (Part 1).json', 'w', encoding='utf-8') as file:
+    json.dump(hospital_info, file, indent=4, ensure_ascii=False)
 
 
